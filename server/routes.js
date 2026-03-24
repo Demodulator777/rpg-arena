60bc2c7b966b4f11df051d2a03de32065049ccf4
Demodulator777 <83093957+Demodulator777@users.noreply.github.com>
2026-03-24 11:11:37 +0200

    Update routes.js
    ringading

--- server/routes.js
+++ server/routes.js
@@ -953,7 +953,7 @@ function generateBackendRandomItem(level, type) {
     const emoji  = generator.emojis[Math.floor(Math.random() * generator.emojis.length)];
     const imgSlug = name.toLowerCase().replace(/\s+/g, '-');
     
-    const slotMap = { weapon:'weapon', armor:'armor', helmet:'helmet', shield:'shield', accessory:'accessory', amulet:'amulet', ring:'ring', boots:'boots' };
+    const slotMap = { weapon:'weapon', armor:'armor', helmet:'helmet', shield:'shield', accessory:'accessory', amulet:'ring', ring:'ring', boots:'boots' };
 
     const item = {
         id:      `${type}_${Date.now()}_${Math.random().toString(36).substr(2,9)}`,
