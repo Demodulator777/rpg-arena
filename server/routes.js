f895f424d3ce9df86011d7f243878b3af0c8a216
Demodulator777 <83093957+Demodulator777@users.noreply.github.com>
2026-04-08 07:20:35 +0300

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1905,12 +1905,14 @@ router.post('/missions/collect', auth, async (req, res) => {
         const skillMods     = computeClassModifiers(freshChar.class, learnedIds);
         
         // Rogue no-shield agility bonus
-        let noShieldAgiBonus = 0;
-        if (freshChar.class === 'rogue') {
-            const equipped = await getEquippedItems(db, freshChar.id);
-            const hasShield = !!equipped.shield;
-            if (!hasShield) noShieldAgiBonus = 5;
-        }
+let noShieldAgiBonus = 0;
+if (char.class === 'rogue') {
+    const hasShield = !!equippedObj.shield;
+    if (!hasShield) {
+        // 5% agility bonus when no shield equipped
+        noShieldAgiBonus = Math.floor((char.agility || 0) * 0.05);
+    }
+}
         
         const playerFighter = {
             id: freshChar.id,
