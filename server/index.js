6fa49c46ee7b616644dfbc8f5aba43a4f5e8cde2
Demodulator777 <83093957+Demodulator777@users.noreply.github.com>
2026-04-21 14:30:39 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ const { getDb } = require('./db');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   const cspDirectives = [
     "default-src 'self'",
