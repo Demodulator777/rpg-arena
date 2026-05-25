0a7a9ac0fd9d81670577c4b842cb3e388e1530ec
Demodulator777 <83093957+Demodulator777@users.noreply.github.com>
2026-05-25 11:32:47 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
 const cspDirectives = [
     "default-src 'self'",
-    "script-src 'self' 'unsafe-eval'",
+    "script-src 'self'",
     "connect-src 'self'",
     "img-src 'self' data: blob:",
     "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
