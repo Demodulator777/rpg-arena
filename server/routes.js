fb783ce60b874947f2caa132007902479c82b402
Demodulator777 <83093957+Demodulator777@users.noreply.github.com>
2026-06-01 17:07:29 +0300

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -13769,4 +13769,4 @@ async function isCharacterTraining(db, characterId) {
     return !!training;
 }
 
-module.exports = { router, parseAdminPassword };
+module.exports = { router, parseAdminPassword, dbGet, getDb };
