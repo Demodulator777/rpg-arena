9ae78b19ca449f923eb3c08326cb1b06c15e2baf
Demodulator777 <83093957+Demodulator777@users.noreply.github.com>
2026-07-10 07:36:11 +0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -8639,7 +8639,7 @@ async function openProfile(id) {
         let finalDmgMin = baseDmgMin + gearDmgMin;
         let finalDmgMax = baseDmgMax + gearDmgMax;
 
-    if (c.class === 'warrior') {
+    if (p.class === 'warrior') {
             const strBonus = Math.floor(totalStr / 10);
             finalDmgMin += strBonus;
             finalDmgMax += strBonus;
