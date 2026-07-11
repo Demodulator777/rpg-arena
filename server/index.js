d72cd5082a7dd29382dcdf17bbfce816791a1834
Demodulator777 <83093957+Demodulator777@users.noreply.github.com>
2026-07-11 23:06:32 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const { getDb } = require('./db');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // Favicon (silence 404)
