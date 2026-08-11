f7533d67e2148afe852a1e6ad056b19912770c67
Demodulator777 <83093957+Demodulator777@users.noreply.github.com>
2026-08-11 14:34:25 +0300

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -9814,7 +9814,8 @@ async function buildCharacterResponse(char, db) {
 
     let elemental = null;
     if (await isFeatureEnabled(db, 'spirit_beast_enabled')) {
-        elemental = await ensureElemental(db, char.id);
+        _beastStats = await beastStatBonus(db, char.id);
+        console.error(`[DEBUG MISSION] charId=${char.id}, _beastStats=`, _beastStats);
     }
     // Spirit Beast flat stat bonuses (single source of truth: beastBonusFromRow)
     let beastStrBonus = 0, beastDefBonus = 0, beastMagBonus = 0, beastVitBonus = 0;
